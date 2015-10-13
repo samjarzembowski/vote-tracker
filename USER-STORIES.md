2a49bd7371e79237fbf9e07f980b4b6bff9a33f8
samjarzembowski <sjarzembowski@gmail.com>
2015-10-12 17:07:00 -0700

    initial commit of project.  Struggling with conceptualization and javascript.

---
+++ USER-STORIES.md
@@ -0,0 +1,7 @@
+As a hungry student, I can fantasize about all kinds of different burgs.
+
+As a brewpub chef, I can get ideas for new burgs.
+
+As a parent, I can get new ideas for feeding burgs to my kids.
+
+As a child, I can bug my parents to make my favorite burg.
---
+++ app.js
@@ -0,0 +1,42 @@
+var ImageMaker = function(path) {
+  this.path = path;
+  this.votes = 0;
+  //this.somethingElse = ????
+}
+
+//  for (var i=0; i < 20; i++) {
+//   look at the img directory and push file path to burgArray[i]
+// }
+
+
+// var burgArray = [
+//   'img/burg1.jpg',
+//   'img/burg10.jpeg',
+//   'img/burg11.jpg',
+//   'img/burg12.jpg',
+//   'img/burg13.jpg',
+//   'img/burg14.jpg',
+//   'img/burg15.jpg',
+//   'img/burg16.jpg',
+//   'img/burg17.jpg',
+//   'img/burg18.jpg',
+//   'img/burg19.jpg',
+//   'img/burg2.jpg',
+//   'img/burg20.jpg',
+//   'img/burg3.png',
+//   'img/burg4.jpg',
+//   'img/burg5.jpg',
+//   'img/burg6.jpg',
+//   'img/burg7.png',
+//   'img/burg8.jpg',
+//   'img/burg9.jpeg',
+// ];
+
+// for (var i = 0; i < burgArray.length; i++) {
+//   var burgObj = new ImageMaker(burgArray[i]);
+//   console.log(burgObj);
+// }
+
+var burgArray = [array of ]
+
+
